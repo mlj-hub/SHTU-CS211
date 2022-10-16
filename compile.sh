1d98bb4145746fd04eaca00cfb753ce5f2585c15
malj <malj@shanghaitech.edu.cn>
2022-10-16 23:39:26 +0800

    update compile script since change of platform

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 # ！/bin/sh
-gcc-riscv -march=rv64ia ~/workspace/cs211/lab/lab0/RISCV-Simulator/test/lib.c ~/workspace/cs211/lab/lab0/RISCV-Simulator/test/lab0.c -o ~/workspace/cs211/lab/lab0/RISCV-Simulator/riscv-elf/lab0.riscv
-gcc-riscv-obj -D ~/workspace/cs211/lab/lab0/RISCV-Simulator/riscv-elf/lab0.riscv > ~/workspace/cs211/lab/lab0/RISCV-Simulator/riscv-elf/lab0.s
+gcc-riscv -march=rv64ia ~/RISCV-Simulator/test/lib.c ~/RISCV-Simulator/test/lab0.c -o ~/RISCV-Simulator/riscv-elf/lab0.riscv
+gcc-riscv-obj -D ~/RISCV-Simulator/riscv-elf/lab0.riscv > ~/RISCV-Simulator/riscv-elf/lab0.s
