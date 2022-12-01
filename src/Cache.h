96c14125c4835011985c9f3e1f167d24960ed79c
malj <malj@shanghaitech.edu.cn>
2022-12-01 18:19:36 +0800

    add directory maintain in cache

--- src/Cache.h
+++ src/Cache.h
@@ -11,11 +11,30 @@
 #include <vector>
 
 #include "MemoryManager.h"
+#include "State.h"
 
 class MemoryManager;
 
+struct Block {
+  bool valid;
+  bool modified;
+  uint32_t tag;
+  uint32_t id;
+  uint32_t size;
+  uint32_t lastReference;
+  std::vector<uint8_t> data;
+  State state;
+  Block():valid(0),modified(0),tag(0),id(0),size(0),lastReference(0),state(State::Invalid) {}
+  Block(const Block &b)
+      : valid(b.valid), modified(b.modified), tag(b.tag), id(b.id),
+        size(b.size),state(b.state) {
+    data = b.data;
+  }
+};
+
 class Cache {
 public:
+
   struct Policy {
     // In bytes, must be power of 2
     uint32_t cacheSize;
@@ -26,21 +45,7 @@ public:
     uint32_t missLatency; // in cycles
   };
 
-  struct Block {
-    bool valid;
-    bool modified;
-    uint32_t tag;
-    uint32_t id;
-    uint32_t size;
-    uint32_t lastReference;
-    std::vector<uint8_t> data;
-    Block():valid(0),modified(0),tag(0),id(0),size(0),lastReference(0) {}
-    Block(const Block &b)
-        : valid(b.valid), modified(b.modified), tag(b.tag), id(b.id),
-          size(b.size) {
-      data = b.data;
-    }
-  };
+
 
   struct Statistics {
     uint32_t numRead;
@@ -50,20 +55,30 @@ public:
     uint64_t totalCycles;
   };
 
-  Cache(MemoryManager *manager, Policy policy, Cache *lowerCache = nullptr,
+  Cache(MemoryManager *manager, Policy policy,uint8_t level,Cache *lowerCache = nullptr,pthread_mutex_t* lock=nullptr,
         bool writeBack = true, bool writeAllocate = true);
 
   bool inCache(uint32_t addr);
+  bool inShared(uint32_t addr);
   uint32_t getBlockId(uint32_t addr);
   uint8_t getByte(uint32_t addr, uint32_t *cycles = nullptr);
   void setByte(uint32_t addr, uint8_t val, uint32_t *cycles = nullptr);
+  void passSharedBlockToHigherLevel(uint32_t addr, Block * block, uint32_t * cycles);
 
   void printInfo(bool verbose);
   void printStatistics();
 
-  Statistics statistics;
+  State getBlockState(uint32_t addr);
+  void invalidCacheBlock(uint32_t addr);
+  void setState(uint32_t addr,State state);
+  void findSetStateAll(uint32_t addr, State state,std::vector<uint8_t> &data);
+  void setStateAll(uint32_t addr,State state);
+  uint32_t getReplacementBlockId(uint32_t begin, uint32_t end);
+  uint32_t getReplacementBlockIdLLC(uint32_t begin, uint32_t end);
 
-private:
+  Statistics statistics;
+  uint8_t level;
+
   uint32_t referenceCounter;
   bool writeBack;     // default true
   bool writeAllocate; // default true
@@ -71,11 +86,18 @@ private:
   Cache *lowerCache;
   Policy policy;
   std::vector<Block> blocks;
+  pthread_mutex_t *lock;
+
+private:
 
   void initCache();
   void loadBlockFromLowerLevel(uint32_t addr, uint32_t *cycles = nullptr);
-  uint32_t getReplacementBlockId(uint32_t begin, uint32_t end);
+  void loadSharedBlockFromLowerLevel(uint32_t addr, uint32_t *cycles = nullptr);
+
   void writeBlockToLowerLevel(Block &b);
+  void handleWriteBack(Block b);
+  void handleLLCWriteBack(Block b);
+  void handlePrivateReadHit(uint32_t addr,Block*dst,Block*src);
 
   // Utility Functions
   bool isPolicyValid();
